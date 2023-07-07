c36719d675f18557026332f101bdcf3162385d22
Vaibhav Ahuja <85643531+vaibhav5140@users.noreply.github.com>
2023-07-08 01:26:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app=express();
 app.use(express.json({limit:"10mb"}));
 app.use(morgan("dev"));
 app.use(cors());
-app.get("/api",(req,res)=>{res.json({message:'HI FROM SERVER'})});
+app.get("/",(req,res)=>{res.json({message:'HI FROM SERVER'})});
 app.use('/api',authroutes);
 
 app.use('/api',adroutes);
